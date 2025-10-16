093b9b3273190b086bba51568fd62ccf307ae248
Alex Lai <alex_lai@accton.com>
2025-10-16 08:25:53 +0000

    [Accton][as7726-32x] Add onlp_data_path_reset() to support chip reset functionality
    * Implements warm reset for MAC.
    * @param unit_id: The warm reset device unit ID, should be 0.
    * @param reset_dev: The warm reset device ID, should be 1 to (WARM_RESET_MAX-1).
    * @param ret: Return value.
    
    Signed-off-by: Alex Lai <alex_lai@accton.com>

--- packages/platforms/accton/x86-64/as7726-32x/onlp/builds/x86_64_accton_as7726_32x/module/src/platform_lib.h
+++ packages/platforms/accton/x86-64/as7726-32x/onlp/builds/x86_64_accton_as7726_32x/module/src/platform_lib.h
@@ -68,6 +68,8 @@
 
 #define IDPROM_PATH "/sys/class/i2c-adapter/i2c-0/0-0056/eeprom"
 
+#define WARM_RESET_FORMAT "/sys/bus/i2c/devices/11-0060/reset_mac"
+
 int onlp_file_write_integer(char *filename, int value);
 int onlp_file_read_binary(char *filename, char *buffer, int buf_size, int data_len);
 int onlp_file_read_string(char *filename, char *buffer, int buf_size, int data_len);
@@ -86,6 +88,11 @@ typedef enum psu_type {
     PSU_TYPE_DC_48V_F2B,
     PSU_TYPE_DC_48V_B2F
 } psu_type_t;
+
+enum reset_dev_type {
+    WARM_RESET_MAC = 1,
+    WARM_RESET_MAX
+};
 
 psu_type_t get_psu_type(int id, char* modelname, int modelname_len);
 int psu_pmbus_serial_number_get(int id, char *serial, int serial_len);
